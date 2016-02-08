371c5a558c8a82d47c9d0acc0c60e69e2693e400
Ilya Puchka <ip@hellofresh.com>
2016-02-08 11:44:20 +0100

    increased required version of Dip to 4.2.0
    Dip starts to support spm only in 4.2.0

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
   name: "DipUI",
   targets: [],
   dependencies: [
-    .Package(url: "https://github.com/AliSoftware/Dip", majorVersion: 4)
+    .Package(url: "https://github.com/AliSoftware/Dip", majorVersion: 4, minor: 2)
   ]
 )
