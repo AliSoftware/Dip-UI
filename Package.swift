0fe8b03fa2a366ae355e8a9bcea27ba143597194
Ilya Puchka <ilyapuchka@gmail.com>
2016-02-25 00:35:18 +0100

    improved package description

--- Package.swift
+++ Package.swift
@@ -2,8 +2,7 @@ import PackageDescription
 
 let package = Package(
   name: "DipUI",
-  targets: [],
   dependencies: [
-    .Package(url: "https://github.com/AliSoftware/Dip", majorVersion: 4, minor: 2)
+    .Package(url: "https://github.com/AliSoftware/Dip.git", "4.2.0")
   ]
 )
